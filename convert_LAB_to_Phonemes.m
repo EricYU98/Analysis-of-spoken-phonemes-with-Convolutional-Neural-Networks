e19ea3a970961f9ad14e57a0e115302074b7c3f7
EricYU98 <57226663+EricYU98@users.noreply.github.com>
2020-01-16 20:25:41 +0000

    Add files via upload

---
+++ convert_LAB_to_Phonemes.m
@@ -0,0 +1,21 @@
+%% convert lab to matlab cell
+function [Phonemes,numPhonemes] = convert_LAB_to_Phonemes(LAB)
+
+% Input may be a cell with the labelled phonemes or just the file from which these
+% must be read
+if ~isa(LAB,'cell')
+    LAB = importdata(LAB,' ');
+end
+    %% Interpret Labelled Phonemes
+    % Read the numLines    
+    numPhonemes             = size(LAB,1);
+    Phonemes{numPhonemes,3}     = '';
+    for k=1:numPhonemes
+        % Each line is a {start end phoneme}
+        currLine            = LAB{k};
+        placesSpace         = strfind(currLine,' ');
+        Phonemes{k,1}           = str2double(currLine(1:placesSpace(1)-1));
+        Phonemes{k,2}           = str2double(currLine(placesSpace(1)+1:placesSpace(2)-1));
+        Phonemes{k,3}           = (currLine(placesSpace(2)+1:end));
+    end
+end
